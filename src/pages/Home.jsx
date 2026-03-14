99ed510fe03b3e62b6b3fddef329221ba88277d3
Mamakbubu <maboelatk@gmail.com>
2026-03-14 22:35:22 +0200

    2nd commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
+import { NavBar } from "../components/NavBar";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
 export const Home = () => {
     return (
         <div className="min-h-screen">
             {/* Theme toggle */}
+            <ThemeToggle />
 
             {/* Background Effects */}
-
+            <StarBackground />
             {/* Navbar */}
+            <NavBar />
 
             {/* Main Content */}
 
